86cfcb83dda30213f0933c44a30da18e13267c5d
SDMckone <seandmckone@gmail.com>
2022-05-20 16:35:21 -0400

    Complete UI

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # odin-rps
 Rock-paper-scissors game for odin project
+
+Icons courtesey of flaticon.com, created by Freepik
--- myscripts.js
+++ myscripts.js
@@ -13,6 +13,9 @@ function playRound(playerInput, cpuInput){
     playerInput = playerInput.toLowerCase();
     cpuInput = cpuInput.toLowerCase(); 
 
+    document.getElementsByClassName("playerselectionimg")[0].src = `./images/${playerInput}.png`
+    document.getElementsByClassName("computerselectionimg")[0].src = `./images/${cpuInput}.png`
+
     if(playerInput === cpuInput){
         return `Tie! You both chose ${playerInput}!`;
     }
@@ -36,32 +39,115 @@ function playRound(playerInput, cpuInput){
     }
 }
 
-function game() {
-    playerScore = 0;
-    opponentScore = 0;
 
-    for (let i = 0; i < 5; i++) {
-        playerInput = prompt("Choose rock, paper, or scissors");
-        const computerInput = computerPlay();
-        result = playRound(playerInput, computerInput);
-        if(result.includes("win")) {
-            playerScore++;
-        }
-        else if (result.includes("lose")) {
-            opponentScore++;
-        }
-        console.log(result);
+// Player and computer scores
+let playerScore = 0;
+let computerScore = 0;
+
+// Selectors for information section
+let playerselection = document.querySelector('.playerselectionimg');
+let computerselection = document.querySelector('.computerselectionimg');
+let score = document.querySelector('.scorecount');
+let results = document.querySelector('.results');
+
+// Selectors for buttons
+let rockbutton = document.querySelector('.rockbutton'); 
+let paperbutton = document.querySelector('.paperbutton');
+let scissorsbutton = document.querySelector('.scissorsbutton');
+
+
+rockbutton.addEventListener('mousedown', () => {
+    rockbutton.classList.toggle("pressed");
+    playerInput = "rock"
+    const computerInput = computerPlay();
+    result = playRound(playerInput, computerInput);
+
+    if(result.includes("win")) {
+        playerScore++;
+    }
+    else if (result.includes("lose")) {
+        computerScore++;
+    }
+    results.textContent = result;
+    score.textContent = `${playerScore}:${computerScore}`;
+
+    if ( playerScore == 5) {
+        results.textContent = "You Win! Click a button to play again.";
+        playerScore = 0;
+        computerScore = 0;
     }
 
-    if (playerScore > opponentScore){
-        console.log("You win!");
+    if ( computerScore == 5) {
+        results.textContent = "You Lose! Click a button to play again.";
+        playerScore = 0;
+        computerScore = 0;
     }
-    else if (playerScore < opponentScore){
-        console.log("You lose!");
-    }
-    else {
-        console.log("Tie!")
-    }
-}
+    
+});
+rockbutton.addEventListener('mouseup', () => {
+    rockbutton.classList.toggle("pressed");
+});
 
-game();
+
+paperbutton.addEventListener('mousedown', () => {
+    paperbutton.classList.toggle("pressed");
+    playerInput = "paper"
+    const computerInput = computerPlay();
+    result = playRound(playerInput, computerInput);
+
+    if(result.includes("win")) {
+        playerScore++;
+    }
+    else if (result.includes("lose")) {
+        computerScore++;
+    }
+    results.textContent = result;
+    score.textContent = `${playerScore}:${computerScore}`;
+
+    if ( playerScore == 5) {
+        results.textContent = "You Win! Click a button to play again.";
+        playerScore = 0;
+        computerScore = 0;
+    }
+
+    if ( computerScore == 5) {
+        results.textContent = "You Lose! Click a button to play again.";
+        playerScore = 0;
+        computerScore = 0;
+    }
+});
+paperbutton.addEventListener('mouseup', () => {
+    paperbutton.classList.toggle("pressed");
+});
+
+scissorsbutton.addEventListener('mousedown', () => {
+    scissorsbutton.classList.toggle("pressed");
+    playerInput = "scissors"
+    const computerInput = computerPlay();
+    result = playRound(playerInput, computerInput);
+
+    if(result.includes("win")) {
+        playerScore++;
+    }
+    else if (result.includes("lose")) {
+        computerScore++;
+    }
+    results.textContent = result;
+    score.textContent = `${playerScore}:${computerScore}`;
+
+    if ( playerScore == 5) {
+        results.textContent = "You Win! Click a button to play again.";
+        playerScore = 0;
+        computerScore = 0;
+    }
+
+    if ( computerScore == 5) {
+        results.textContent = "You Lose! Click a button to play again.";
+        playerScore = 0;
+        computerScore = 0;
+    }
+
+});
+scissorsbutton.addEventListener('mouseup', () => {
+    scissorsbutton.classList.toggle("pressed");
+});
